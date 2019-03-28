8f6b66908f1a7c5bfcd9d39c20d97dae1be90f68
Hannah Huggins <dowdallhannah@yahoo.com>
2019-03-28 13:10:09 +0000

    Notes on javascript console

---
+++ scripts.js
@@ -0,0 +1,41 @@
+// JavaScript everywhere vid 
+
+// ---------------------------------------------------------------------------------------------------------------------------------//
+
+// About JavaScript basics vid 
+
+//Syntax = programming language's vocab and grammer 
+//Running/executing the program = the browser reading the code
+//
+//
+//
+
+// ---------------------------------------------------------------------------------------------------------------------------------//
+
+// Your first JavaScript program vid
+
+// alert("Hello from Treehouse"); - javascript statement 
+
+/* alert("hello");
+document.write("<h1> welcome to javascript basics </h1>"); - writing directly into the webpage
+alert("thanks for visiting"); */
+
+//---------------------------------------------------------------------------------------------------------------------------------//
+
+// Where does JavaScript go vid
+
+// The browser is the easiest place to try out javascript functions. Every browser has a javascript intrepreter which can read the js.
+
+// alert("Hey der");
+
+
+// moving the javascript tag to the bottom of the page allows the html to load first before the js 
+// ---------------------------------------------------------------------------------------------------------------------------------//
+
+// The javaScript console vid 
+
+// You can use the console to help you find errors in the code 
+
+
+
+// ---------------------------------------------------------------------------------------------------------------------------------//
