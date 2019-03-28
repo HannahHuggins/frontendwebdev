a584c2311bc8c92f6313149f86ca047251213f97
Hannah Huggins <dowdallhannah@yahoo.com>
2019-03-28 14:37:11 +0000

    notes

--- dist/scripts.b71a6038.js
+++ dist/scripts.b71a6038.js
@@ -127,8 +127,71 @@ alert("thanks for visiting"); */
 // moving the javascript tag to the bottom of the page allows the html to load first before the js 
 // ---------------------------------------------------------------------------------------------------------------------------------//
 // The javaScript console vid 
-// You can use the console to help you find errors in the code 
+// You can use the console to help you find errors in the code. Console.log lets you print messages within the console
+// console.log("done!");
 // ---------------------------------------------------------------------------------------------------------------------------------//
+// The console challenge vid
+// The console challenge answer vid 
+// ---------------------------------------------------------------------------------------------------------------------------------//
+// Introducing variables vid
+// variables help keep track of info in the program. 'var' creates the box to hold the variable 
+// var score = 0; // assigning a value to a variable 
+// var message = "Hello!";
+// alert(message);
+// message = "welcome to javascript basics";
+// alert(message);
+// ---------------------------------------------------------------------------------------------------------------------------------//
+// Naming variables vid
+
+/* js has a list of names that you can't use for variable names
+names can't start with a number. can only contain letters, numbers, $ and _ 
+variables can be camelcase.
+Variable names need to be descriptive so you know what they're used for 
+*/
+// ---------------------------------------------------------------------------------------------------------------------------------//
+// Strings and numbers vid 
+// A value is what is stored in a variable 
+// var message = "hello";
+// alert(message);
+//---------------------------------------------------------------------------------------------------------------------------------//
+// Spaces, tabs and new lines in JavaScript vid 
+//---------------------------------------------------------------------------------------------------------------------------------//
+// Capturing visitor input vid 
+// prompt("what is your name?");
+// teh browser captures the users response and stores the value. This can be stored in a variable 
+// var visitorName = prompt("what's your name?");
+// alert(visitorName);
+//---------------------------------------------------------------------------------------------------------------------------------//
+// Combining strings - concatenation (adding strings)
+// var visitor = prompt("what's your name?");
+// var message = "hello " + visitor;
+// document.write(message);
+// combining strings like this makes it easier to read.""
+// var visitor = prompt("what's your name?");
+// var message = "Hello " + visitor + ". Welcome to Treehouse.";
+// message += "We are so glad that you came to visit.";
+// message += ". Please come again.";
+// document.write(message);
+//---------------------------------------------------------------------------------------------------------------------------------//
+// Working with strings and finding help vid 
+// var passPhrase = "open sesame";
+// console.log(passPhrase.length); // .length property/method will find the length of the string 
+// //passPhrase is the object and .length is the property. 
+// var otherVariable = "HELLO";
+// console.log(otherVariable.toLowerCase()); //hello
+// console.log(otherVariable); // this will still print HELLO
+// var secondVariable = "yoyoyo";
+// console.log(secondVariable.toUpperCase());
+//---------------------------------------------------------------------------------------------------------------------------------//
+// The variable challenge vid
+// var name = prompt("what's your name?");
+// alert("Hello " + name);
+// var wellness = prompt("How are you today?");
+// alert("glad to hear you are " + wellness);
+// var allResponses = name + wellness;
+// document.write(allResponses);
+// The variable challenge solution
+//---------------------------------------------------------------------------------------------------------------------------------//
 },{}],"../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -156,7 +219,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55810" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56721" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
