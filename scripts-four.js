1ab2b9de68eee305b7b22a61ac3436191ca710fe
Hannah Huggins <dowdallhannah@yahoo.com>
2019-03-29 16:38:00 +0000

    more notes on functions

---
+++ scripts-four.js
@@ -0,0 +1,34 @@
+//Introducing functions vid 
+
+//start by declaring the function (providing instructions)
+//then place a set of parenthesis, this is how functions are identified
+//then braces, forming the code block 
+//js inside the braces is run whenever the function is activated
+//to activate, you 'call' the function 
+
+// function goToCoffeeShop(){
+
+// }
+
+// goToCoffeeShop(); //calling the function 
+
+
+function alertRandom(){
+    var randomNumber = Math.floor(Math.random()*6)+1;
+    alert(randomNumber);
+}
+
+alertRandom();
+
+
+// function expression - assign a function to a variable
+
+var alertRandom = function() {
+    var randonNumber= math.foor(Math.random()*6)+1;
+    alert(randomNumber);
+};
+
+//---------------------------------------------------------------------------------------------------------------------------------//
+
+
+//Functions change the flow of your program vid 
