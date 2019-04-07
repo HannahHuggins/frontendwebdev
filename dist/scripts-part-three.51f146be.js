5fe3a218a496cba70ddf2e5b8e8e7aff2fe0b667
Hannah Huggins <dowdallhannah@yahoo.com>
2019-04-07 17:34:04 +0100

    notes on for in loops with objects

--- dist/scripts-part-three.51f146be.js
+++ dist/scripts-part-three.51f146be.js
@@ -104,222 +104,94 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   // Override the current require with this new one
   return newRequire;
-})({"../../../../../usr/local/lib/node_modules/parcel-bundler/node_modules/constants-browserify/constants.json":[function(require,module,exports) {
-module.exports = {
-  "O_RDONLY": 0,
-  "O_WRONLY": 1,
-  "O_RDWR": 2,
-  "S_IFMT": 61440,
-  "S_IFREG": 32768,
-  "S_IFDIR": 16384,
-  "S_IFCHR": 8192,
-  "S_IFBLK": 24576,
-  "S_IFIFO": 4096,
-  "S_IFLNK": 40960,
-  "S_IFSOCK": 49152,
-  "O_CREAT": 512,
-  "O_EXCL": 2048,
-  "O_NOCTTY": 131072,
-  "O_TRUNC": 1024,
-  "O_APPEND": 8,
-  "O_DIRECTORY": 1048576,
-  "O_NOFOLLOW": 256,
-  "O_SYNC": 128,
-  "O_SYMLINK": 2097152,
-  "O_NONBLOCK": 4,
-  "S_IRWXU": 448,
-  "S_IRUSR": 256,
-  "S_IWUSR": 128,
-  "S_IXUSR": 64,
-  "S_IRWXG": 56,
-  "S_IRGRP": 32,
-  "S_IWGRP": 16,
-  "S_IXGRP": 8,
-  "S_IRWXO": 7,
-  "S_IROTH": 4,
-  "S_IWOTH": 2,
-  "S_IXOTH": 1,
-  "E2BIG": 7,
-  "EACCES": 13,
-  "EADDRINUSE": 48,
-  "EADDRNOTAVAIL": 49,
-  "EAFNOSUPPORT": 47,
-  "EAGAIN": 35,
-  "EALREADY": 37,
-  "EBADF": 9,
-  "EBADMSG": 94,
-  "EBUSY": 16,
-  "ECANCELED": 89,
-  "ECHILD": 10,
-  "ECONNABORTED": 53,
-  "ECONNREFUSED": 61,
-  "ECONNRESET": 54,
-  "EDEADLK": 11,
-  "EDESTADDRREQ": 39,
-  "EDOM": 33,
-  "EDQUOT": 69,
-  "EEXIST": 17,
-  "EFAULT": 14,
-  "EFBIG": 27,
-  "EHOSTUNREACH": 65,
-  "EIDRM": 90,
-  "EILSEQ": 92,
-  "EINPROGRESS": 36,
-  "EINTR": 4,
-  "EINVAL": 22,
-  "EIO": 5,
-  "EISCONN": 56,
-  "EISDIR": 21,
-  "ELOOP": 62,
-  "EMFILE": 24,
-  "EMLINK": 31,
-  "EMSGSIZE": 40,
-  "EMULTIHOP": 95,
-  "ENAMETOOLONG": 63,
-  "ENETDOWN": 50,
-  "ENETRESET": 52,
-  "ENETUNREACH": 51,
-  "ENFILE": 23,
-  "ENOBUFS": 55,
-  "ENODATA": 96,
-  "ENODEV": 19,
-  "ENOENT": 2,
-  "ENOEXEC": 8,
-  "ENOLCK": 77,
-  "ENOLINK": 97,
-  "ENOMEM": 12,
-  "ENOMSG": 91,
-  "ENOPROTOOPT": 42,
-  "ENOSPC": 28,
-  "ENOSR": 98,
-  "ENOSTR": 99,
-  "ENOSYS": 78,
-  "ENOTCONN": 57,
-  "ENOTDIR": 20,
-  "ENOTEMPTY": 66,
-  "ENOTSOCK": 38,
-  "ENOTSUP": 45,
-  "ENOTTY": 25,
-  "ENXIO": 6,
-  "EOPNOTSUPP": 102,
-  "EOVERFLOW": 84,
-  "EPERM": 1,
-  "EPIPE": 32,
-  "EPROTO": 100,
-  "EPROTONOSUPPORT": 43,
-  "EPROTOTYPE": 41,
-  "ERANGE": 34,
-  "EROFS": 30,
-  "ESPIPE": 29,
-  "ESRCH": 3,
-  "ESTALE": 70,
-  "ETIME": 101,
-  "ETIMEDOUT": 60,
-  "ETXTBSY": 26,
-  "EWOULDBLOCK": 35,
-  "EXDEV": 18,
-  "SIGHUP": 1,
-  "SIGINT": 2,
-  "SIGQUIT": 3,
-  "SIGILL": 4,
-  "SIGTRAP": 5,
-  "SIGABRT": 6,
-  "SIGIOT": 6,
-  "SIGBUS": 10,
-  "SIGFPE": 8,
-  "SIGKILL": 9,
-  "SIGUSR1": 30,
-  "SIGSEGV": 11,
-  "SIGUSR2": 31,
-  "SIGPIPE": 13,
-  "SIGALRM": 14,
-  "SIGTERM": 15,
-  "SIGCHLD": 20,
-  "SIGCONT": 19,
-  "SIGSTOP": 17,
-  "SIGTSTP": 18,
-  "SIGTTIN": 21,
-  "SIGTTOU": 22,
-  "SIGURG": 16,
-  "SIGXCPU": 24,
-  "SIGXFSZ": 25,
-  "SIGVTALRM": 26,
-  "SIGPROF": 27,
-  "SIGWINCH": 28,
-  "SIGIO": 23,
-  "SIGSYS": 12,
-  "SSL_OP_ALL": 2147486719,
-  "SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION": 262144,
-  "SSL_OP_CIPHER_SERVER_PREFERENCE": 4194304,
-  "SSL_OP_CISCO_ANYCONNECT": 32768,
-  "SSL_OP_COOKIE_EXCHANGE": 8192,
-  "SSL_OP_CRYPTOPRO_TLSEXT_BUG": 2147483648,
-  "SSL_OP_DONT_INSERT_EMPTY_FRAGMENTS": 2048,
-  "SSL_OP_EPHEMERAL_RSA": 0,
-  "SSL_OP_LEGACY_SERVER_CONNECT": 4,
-  "SSL_OP_MICROSOFT_BIG_SSLV3_BUFFER": 32,
-  "SSL_OP_MICROSOFT_SESS_ID_BUG": 1,
-  "SSL_OP_MSIE_SSLV2_RSA_PADDING": 0,
-  "SSL_OP_NETSCAPE_CA_DN_BUG": 536870912,
-  "SSL_OP_NETSCAPE_CHALLENGE_BUG": 2,
-  "SSL_OP_NETSCAPE_DEMO_CIPHER_CHANGE_BUG": 1073741824,
-  "SSL_OP_NETSCAPE_REUSE_CIPHER_CHANGE_BUG": 8,
-  "SSL_OP_NO_COMPRESSION": 131072,
-  "SSL_OP_NO_QUERY_MTU": 4096,
-  "SSL_OP_NO_SESSION_RESUMPTION_ON_RENEGOTIATION": 65536,
-  "SSL_OP_NO_SSLv2": 16777216,
-  "SSL_OP_NO_SSLv3": 33554432,
-  "SSL_OP_NO_TICKET": 16384,
-  "SSL_OP_NO_TLSv1": 67108864,
-  "SSL_OP_NO_TLSv1_1": 268435456,
-  "SSL_OP_NO_TLSv1_2": 134217728,
-  "SSL_OP_PKCS1_CHECK_1": 0,
-  "SSL_OP_PKCS1_CHECK_2": 0,
-  "SSL_OP_SINGLE_DH_USE": 1048576,
-  "SSL_OP_SINGLE_ECDH_USE": 524288,
-  "SSL_OP_SSLEAY_080_CLIENT_DH_BUG": 128,
-  "SSL_OP_SSLREF2_REUSE_CERT_TYPE_BUG": 0,
-  "SSL_OP_TLS_BLOCK_PADDING_BUG": 512,
-  "SSL_OP_TLS_D5_BUG": 256,
-  "SSL_OP_TLS_ROLLBACK_BUG": 8388608,
-  "ENGINE_METHOD_DSA": 2,
-  "ENGINE_METHOD_DH": 4,
-  "ENGINE_METHOD_RAND": 8,
-  "ENGINE_METHOD_ECDH": 16,
-  "ENGINE_METHOD_ECDSA": 32,
-  "ENGINE_METHOD_CIPHERS": 64,
-  "ENGINE_METHOD_DIGESTS": 128,
-  "ENGINE_METHOD_STORE": 256,
-  "ENGINE_METHOD_PKEY_METHS": 512,
-  "ENGINE_METHOD_PKEY_ASN1_METHS": 1024,
-  "ENGINE_METHOD_ALL": 65535,
-  "ENGINE_METHOD_NONE": 0,
-  "DH_CHECK_P_NOT_SAFE_PRIME": 2,
-  "DH_CHECK_P_NOT_PRIME": 1,
-  "DH_UNABLE_TO_CHECK_GENERATOR": 4,
-  "DH_NOT_SUITABLE_GENERATOR": 8,
-  "NPN_ENABLED": 1,
-  "RSA_PKCS1_PADDING": 1,
-  "RSA_SSLV23_PADDING": 2,
-  "RSA_NO_PADDING": 3,
-  "RSA_PKCS1_OAEP_PADDING": 4,
-  "RSA_X931_PADDING": 5,
-  "RSA_PKCS1_PSS_PADDING": 6,
-  "POINT_CONVERSION_COMPRESSED": 2,
-  "POINT_CONVERSION_UNCOMPRESSED": 4,
-  "POINT_CONVERSION_HYBRID": 6,
-  "F_OK": 0,
-  "R_OK": 4,
-  "W_OK": 2,
-  "X_OK": 1,
-  "UV_UDP_REUSEADDR": 4
-}
-;
-},{}],"js-loops-arrays-obects/scripts-part-two.js":[function(require,module,exports) {
-"use strict";
+})({"js-loops-arrays-obects/scripts-part-three.js":[function(require,module,exports) {
+//The Object Literal
+//------------------------------------------------------------------
+// var student = {
+//     name: 'Dave',
+//     grades: [80, 85, 90]
+// };
+//key isn't in quotes, acts like a variable name 
+// var person = {
+//     name : 'Sarah',
+//     country : 'US',
+//     age : 35,
+//     treeHouseStudent : true,
+//     skills : ['JavaScript', 'HTML']
+// };
+//------------------------------------------------------------------
+//Accessing object properties
+//arrays use an index number to access the value, objects use keys
+// var person = {
+//     name: 'Dave',
+//     age: 24
+// };
+// alert(person['name']);
+// provide the propertys name as a string
+//can also use dot notation, easier 
+// alert(person.name);
+// to change the name of the person object, you just assign a new value 
+// person.name = 'Bobby';
+// alert(person.name);
+// person.country = 'Brazil';
+// alert(person.country);
+// var person = {
+//     name: 'sarah',
+//     country: 'us',
+//     age: 35,
+//     treehouse: true,
+//     skills : ['javascript', 'html', 'css']
+// };
+// the object has 5 properties 
+// function print(message){
+//     var div = document.getElementById('ouput');
+//     div.innerHTML = message;
+// }
+// var message = 'hello my name is ' + person.name;
+// message += 'i live in the ' + person.country;
+// person.name = 'rainbow dash';
+// message += 'but i really wish my name was ' + person.name;
+// person.age += 1;
+// message += 'my age is now ' + person.age;
+// message += 'i have ' + person.skills.length + ' skills: ';
+// message += person.skills.join(', ');
+// alert(message);
+//------------------------------------------------------------------
+//Using `for in` to Loop Through an Object's Properties
+// lot of similarities between arrays and objects 
+// because array value has a numeric index, for loops are an easy way to
+// access each item in the array 
+// you can access each value in an object using a for-in loop. (only applies to objects).
+// loops through each key (property name) in the object. 
+var student = {
+  name: 'dave',
+  age: 25
+};
 
-var _constants = require("constants");
-},{"constants":"../../../../../usr/local/lib/node_modules/parcel-bundler/node_modules/constants-browserify/constants.json"}],"../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+for (var age in student) {
+  //do something 
+  alert(student.name);
+} // for each key in student, do something 
+//for - the loop 
+//var - the variable (key name)
+// in - in the object 
+// access property in the for in loop using brackets, can't use dot notation 
+
+
+var person = {
+  name: 'sarah',
+  country: 'us',
+  age: 35,
+  treehouse: true,
+  skills: ['js', 'css', 'html']
+};
+
+for (prop in person) {
+  console.log(prop, ': ', person[prop]);
+} //for - keyword introducing the loop
+//prop acts like a paramete in a function 
+//just like a variable, name it whatever you want
+},{}],"../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -346,7 +218,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52302" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52563" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
@@ -488,5 +360,5 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.parcelRequire, id);
   });
 }
-},{}]},{},["../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js-loops-arrays-obects/scripts-part-two.js"], null)
-//# sourceMappingURL=/scripts-part-two.6040f3d6.map
+},{}]},{},["../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js-loops-arrays-obects/scripts-part-three.js"], null)
+//# sourceMappingURL=/scripts-part-three.51f146be.map
--- js-loops-arrays-obects/scripts-part-three.js
+++ js-loops-arrays-obects/scripts-part-three.js
@@ -30,14 +30,105 @@
 
 //arrays use an index number to access the value, objects use keys
 
-var person = {
-    name: 'Dave',
-    age: 24
-};
+// var person = {
+//     name: 'Dave',
+//     age: 24
+// };
 
-alert(person['name']);
+// alert(person['name']);
 // provide the propertys name as a string
 
 //can also use dot notation, easier 
 
-alert(person.name);
+// alert(person.name);
+
+
+// to change the name of the person object, you just assign a new value 
+
+// person.name = 'Bobby';
+
+// alert(person.name);
+
+// person.country = 'Brazil';
+
+// alert(person.country);
+
+// var person = {
+//     name: 'sarah',
+//     country: 'us',
+//     age: 35,
+//     treehouse: true,
+//     skills : ['javascript', 'html', 'css']
+// };
+
+// the object has 5 properties 
+
+// function print(message){
+//     var div = document.getElementById('ouput');
+//     div.innerHTML = message;
+// }
+
+// var message = 'hello my name is ' + person.name;
+// message += 'i live in the ' + person.country;
+// person.name = 'rainbow dash';
+// message += 'but i really wish my name was ' + person.name;
+// person.age += 1;
+// message += 'my age is now ' + person.age;
+// message += 'i have ' + person.skills.length + ' skills: ';
+// message += person.skills.join(', ');
+// alert(message);
+
+//------------------------------------------------------------------
+
+//Using `for in` to Loop Through an Object's Properties
+
+// lot of similarities between arrays and objects 
+
+// because array value has a numeric index, for loops are an easy way to
+// access each item in the array 
+
+// you can access each value in an object using a for-in loop. (only applies to objects).
+
+// loops through each key (property name) in the object. 
+
+
+// var student = {
+//     name:'dave',
+//     age:25
+// };
+
+
+// for(var age in student){
+//     //do something 
+//     alert(student.name);
+// }
+// for each key in student, do something 
+
+//for - the loop 
+//var - the variable (key name)
+// in - in the object 
+
+// access property in the for in loop using brackets, can't use dot notation 
+
+
+// var person = {
+//     name:'sarah',
+//     country:'us',
+//     age: 35,
+//     treehouse: true,
+//     skills: ['js', 'css','html']
+
+// };
+
+// for (prop in person){
+//     console.log(prop, ': ', person[prop]);
+// }
+
+
+//for - keyword introducing the loop
+//prop acts like a paramete in a function 
+//just like a variable, name it whatever you want 
+
+//------------------------------------------------------------------
+
+//Mixing and Matching Arrays and Objects
