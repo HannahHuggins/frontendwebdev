6352f55725f870de91372133a96e72bc01064f66
Hannah Huggins <dowdallhannah@yahoo.com>
2019-04-08 10:34:58 +0100

    notes about objects, JSON, and complete objects challenge

--- dist/scripts-part-three.51f146be.js
+++ dist/scripts-part-three.51f146be.js
@@ -163,34 +163,102 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 // access each item in the array 
 // you can access each value in an object using a for-in loop. (only applies to objects).
 // loops through each key (property name) in the object. 
-var student = {
-  name: 'dave',
-  age: 25
-};
-
-for (var age in student) {
-  //do something 
-  alert(student.name);
-} // for each key in student, do something 
+// var student = {
+//     name:'dave',
+//     age:25
+// };
+// for(var age in student){
+//     //do something 
+//     alert(student.name);
+// }
+// for each key in student, do something 
 //for - the loop 
 //var - the variable (key name)
 // in - in the object 
 // access property in the for in loop using brackets, can't use dot notation 
-
-
-var person = {
-  name: 'sarah',
-  country: 'us',
-  age: 35,
-  treehouse: true,
-  skills: ['js', 'css', 'html']
-};
-
-for (prop in person) {
-  console.log(prop, ': ', person[prop]);
-} //for - keyword introducing the loop
+// var person = {
+//     name:'sarah',
+//     country:'us',
+//     age: 35,
+//     treehouse: true,
+//     skills: ['js', 'css','html']
+// };
+// for (prop in person){
+//     console.log(prop, ': ', person[prop]);
+// }
+//for - keyword introducing the loop
 //prop acts like a paramete in a function 
-//just like a variable, name it whatever you want
+//just like a variable, name it whatever you want 
+//------------------------------------------------------------------
+// //Mixing and Matching Arrays and Objects
+// var questions = [
+//     { question:'How many states are there in the US?', answer:50},
+//     { question:'How many continents are there?', answer:7 },
+//     { question:'How many legs does an insect have?', answer:6 }
+// ];
+// // turning the prior 2 dimensional array into objects within an array 
+// var correctAnswers = 0;
+// var question;
+// var answer;
+// var response;
+// function print(message){
+//     document.write(message)
+// }
+// for(var i = 0; i < questions; i += 1){
+//     question = questions[i].question;
+//     answer = questions[i].answer;
+//     response = prompt(question);
+//     response = parseInt(response);
+//     if (response === answer){
+//         correctAnswers += 1;
+//     }
+// }
+// // looping through the array, each time through the loop accessing one of the objects in the array 
+// html = "You got " + correctAnswers + " question(s) right."
+// print(html);
+//------------------------------------------------------------------
+//JavaScript object notation 
+// objects are the model for JSON
+//JS object notation 
+//used with ajax to exchange info between the browser and server
+// json format can easily be converted to a javascript object 
+//
+//------------------------------------------------------------------
+// The build an object challenge, part 1 
+// create a script that creates a bunch of student records and prints the records to a webpage 
+// create a data structure to hold info about students 
+var students = [{
+  name: 'dave',
+  track: 'javaScript',
+  achievements: 'js',
+  points: 50
+}, {
+  name: 'henry',
+  track: 'java',
+  achievements: 'j',
+  point: 100
+}, {
+  name: 'hannah',
+  track: 'javaScript',
+  achievements: 'js',
+  points: 200
+}, {
+  name: 'dave',
+  track: 'javascript',
+  achievements: 'js',
+  points: 500
+}, {
+  name: 'jo',
+  track: 'css',
+  achievements: 'c',
+  points: 1000
+}];
+
+for (var prop in students) {
+  console.log(students[prop]);
+}
+
+;
 },{}],"../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -218,7 +286,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52563" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49936" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
--- js-loops-arrays-obects/scripts-part-three.js
+++ js-loops-arrays-obects/scripts-part-three.js
@@ -131,4 +131,68 @@
 
 //------------------------------------------------------------------
 
-//Mixing and Matching Arrays and Objects
+// //Mixing and Matching Arrays and Objects
+
+// var questions = [
+//     { question:'How many states are there in the US?', answer:50},
+//     { question:'How many continents are there?', answer:7 },
+//     { question:'How many legs does an insect have?', answer:6 }
+// ];
+
+// // turning the prior 2 dimensional array into objects within an array 
+
+// var correctAnswers = 0;
+// var question;
+// var answer;
+// var response;
+
+// function print(message){
+//     document.write(message)
+// }
+
+// for(var i = 0; i < questions; i += 1){
+//     question = questions[i].question;
+//     answer = questions[i].answer;
+//     response = prompt(question);
+//     response = parseInt(response);
+//     if (response === answer){
+//         correctAnswers += 1;
+//     }
+// }
+// // looping through the array, each time through the loop accessing one of the objects in the array 
+// html = "You got " + correctAnswers + " question(s) right."
+// print(html);
+
+//------------------------------------------------------------------
+
+//JavaScript object notation 
+
+// objects are the model for JSON
+//JS object notation 
+//used with ajax to exchange info between the browser and server
+ 
+// json format can easily be converted to a javascript object 
+
+//
+//------------------------------------------------------------------
+
+// The build an object challenge, part 1 
+
+// create a script that creates a bunch of student records and prints the records to a webpage 
+
+// create a data structure to hold info about students 
+
+
+var students = [
+    {name: 'dave', track: 'javaScript', achievements: 'js', points:50},
+    {name: 'henry', track: 'java', achievements: 'j', point: 100},
+    {name: 'hannah', track: 'javaScript', achievements: 'js', points: 200},
+    {name: 'dave', track: 'javascript', achievements: 'js', points: 500},
+    {name: 'jo', track: 'css', achievements: 'c', points: 1000}
+];
+
+for (var prop in students){
+    console.log(students[prop])
+};
+
+// the build an object challenge, part 1 solution 
